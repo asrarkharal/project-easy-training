0cfdb96aca5174a5dd2440fac97425194e4e1cab
wwwOANwww <60411997+wwwOANwww@users.noreply.github.com>
2020-02-27 06:20:59 +0100

    Oan latest pages

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,56 @@
-<footer>
-		<button id="myBtn"><a href="#top" style="color: white">top</a></button>
-		
-		<div  >	
-		<a href="index.php" class="button1" >HOME</a>  
-		<a href="#" class="button1">about</a> 		
-		<a href="product_detail.php" class="button1">products</a> 		
-		<a href="contact.php" class="button1">contact</a> 		
+
+<footer class="box10">	
+        <div class="box6" > 		
+              <div  > 
+              <img src="img/logoEhealthy-footer.png"/>  
+            </div>
+                 <div  >
+               
+				  E-healthy.com <i>BE HEALTHY choose E-healthy </i><br> Åsögatan 117, Stockholm<br>	 
 		</div>
-	<div class="footer-small">Copyright @ 2020 :: E-healthy :: All rights reserved #WUE19 </div>
-	</footer>
+		<div class="iconSocial"> 
+		<a href="#!"><img src="img/online_social_media_facebook-128.png"/>  </a>
+		<a href="#!"> <img src="img/twitter_online_social_media-128.png"/>  </a>
+		<a href="#!"> <img src="img/instagram_online_social_media_photo-128.png"/>  </a>
+		<a href="#!"> <img src="img/online_social_media_google_plus-128.png"/>
+		  </div>
+			   
+		<div class="box7"  >
+            <h3>Categories</h3>
+            <ul >
+              <li><a href="#!">Be Inspired</a></li>
+              <li>
+                <a href="#!">Be Cozy</a>
+              </li>
+              <li>
+                <a href="#!">Be Strong</a>
+              </li>
+            </ul>
+          </div>
+
+
+		  <div class="box8"  >
+            <h3>Quick Links</h3>
+            <ul  >
+              <li><a href="#!">About Us</a></li>
+              <li><a href="#!">Contact Us</a></li>
+              <li>
+                <a href="#!">Work with us</a>
+              </li>
+              <li>
+                <a href="register.php">Join us</a>
+              </li>
+            </ul>
+          </div>
+					
+		  </div>
+		  <div class="box9" >
+             
+              Copyright &copy; 2020 All Rights Reserved by
+              <a href="#">E-Healthy.com</a>.
+            
+		  </div>
+		  
+                    
+              
+        </footer> 
